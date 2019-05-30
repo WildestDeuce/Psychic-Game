803d7b836f55a817e35abffc056a2d0bc56cd44a
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-05-30 02:32:44 -0400

    set variables in javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Set variables and arrays
+var win = 0;
+var loss = 0;
+var attempts = 10
+var letters = "qwertyuiopasdfghjklzxcvbnm"
+var letterPick
+
+
+// How the computer will select its letter
+letterPick = letters
