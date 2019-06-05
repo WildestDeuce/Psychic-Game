a034d3e069de301b138446f137b739f3902f0f87
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-06-04 20:19:28 -0400

    getElementById added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var win = 0;
 var loss = 0;
 var attempts = 10;
 var letters = ("abcdefghijklmnopqrstuvwxyz").split("");
-
+var attemptArray = [];
 
 // How the computer will select its letter?
 
@@ -18,41 +18,42 @@ document.onkeyup = function (event) {
 
     var userGuess = event.key;
 
-    // Reset the game after a win/loss
-    function Reset() {
-        if (options.indexOf(userGuess) > -1) {
+    if (letters.indexOf(userGuess) > -1) {
 
-            if (userGuess === letterPick) {
-                win++;
-                attempts = 10;
-                letters = [];
+        // Reset the game after a win/loss
+        function Reset() {
+            if (attemptArray.indexOf(userGuess) > -1) {
+
+                if (userGuess === letterPick) {
+                    win++;
+                    attempts = 10;
+                    attemptArray = []
+                }
+
+                if (userGuess != letterPick) {
+                    attempts--;
+                    attemptArray.push(userGuess);
+                }
+
+                if (attempts === 0) {
+
+                    attempts = 10;
+                    losses++;
+                    attemptArray = [];
+                }
+
             }
 
-            if (userGuess != letterPick) {
-                attempts--;
-                letters.push(userGuess);
-            }
+            //Dispaying to html
 
-            if (attempts === 0) {
+        }
+        document.getElementById("win").textContent = win;
+        document.getElementById("loss").textContent = loss;
+        document.getElementById("attempts").textContent = attempts;
+        document.getElementById("letterPick").textContent = letterPick;
 
-               attempts = 10;
-                losses++;
-                letters = [];
-            }
+        console.log(userGuess);
+        console.log(letterPick);
 
-    //Dispaying to html
-
-    var html =
-
-    "<h1>Psychic Guessing Game</h1>"
-    "<h2>Guess What Letter I'm Thinking Of!</h2>"
-    "<h3>Wins:" + win + "<span id="win"></span></h3>"
-    "<h3>Losses:" + loss+ "<span id="loss"></span></h3>"
-    "<h3>Guesses Remaining:" + attempts + "<span id="attempts"></span></h3>"
-    "<h3>Guesses So Far:" + letterPick + "<span id="letterPick"></span></h3>"
-
-    //document.getElementbyId(game).innerHTML = html or document.querySelector("#game").innerHTML = html;
-
-
-console.log(userGuess)
-console.log(letterPick)
+    }
+}
